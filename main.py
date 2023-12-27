1c2e19c2e45a9d05078e86f57cccb63f0d0263dd
Periphanes <johnwon0723@gmail.com>
2023-12-27 12:48:33 +0900

    Simple Baseline hash model

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 import numpy as np
 import torchvision
 
-import PIL
 import argparse
 import os
 
@@ -17,25 +16,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--seed', type=int, default=1004)
 parser.add_argument('--cpu', type=int, default=0)
 
-parser.add_argument('--batch-size', type=int, defualt=16)
+parser.add_argument('--batch-size', type=int, default=16)
 
 args = parser.parse_args()
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 
 class simpleHashModel(nn.Module):
-    def __init__(self):
+    def __init__(self, args):
         super().__init__()
 
-
-
-
+        self.hash_func = nn.Sequential(
+            nn.Linear(ENCODED_VEC_SIZE_ALEXNET, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        return self.hash_func(x)
 
 def main():
     print("Program Started")
 
     train_dataset = torchvision.datasets.CIFAR10(root="./datasets", train=True, download=True)
     test_dataset = torchvision.datasets.CIFAR10(root="./datasets", train=False, download=True)
+
+    train_selected = []
+    test_selected = []
+
+    for data in train_dataset:
+        if data[1] == 0 or data[1] == 1:
+            train_selected.append(data)
+    for data in test_dataset:
+        if data[1] == 0 or data[1] == 1:
+            test_selected.append(data)
 
     alexnet = torchvision.models.alexnet(weights = torchvision.models.AlexNet_Weights.IMAGENET1K_V1)
     alexnet_weights = torchvision.models.AlexNet_Weights.IMAGENET1K_V1
@@ -61,12 +78,15 @@ def main():
 
             return processed_img, y
     
-    alex_train_dataset = AlexDataset(train_dataset)
-    alex_test_dataset = AlexDataset(test_dataset)
+    alex_train_dataset = AlexDataset(train_selected)
+    alex_test_dataset = AlexDataset(test_selected)
 
     alex_train_loader = torch.utils.data.DataLoader(alex_train_dataset, batch_size=16, shuffle=True, drop_last=True)
     alex_test_loader = torch.utils.data.DataLoader(alex_test_dataset, batch_size=16, shuffle=True, drop_last=True)
 
+    for batch in alex_train_loader:
+        print(batch)
+        exit(0)
 
 
 if __name__ == '__main__':
